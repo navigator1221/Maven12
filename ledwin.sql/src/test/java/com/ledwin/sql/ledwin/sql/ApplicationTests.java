64b67e995defe2753c97b4b9594cb64e818c9ee6
navigator1221 <133993107+navigator1221@users.noreply.github.com>
2023-06-07 09:55:24 +0200

    Add files via upload
    Configuración Maven

---
+++ ledwin.sql/src/test/java/com/ledwin/sql/ledwin/sql/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledwin.sql.ledwin.sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
